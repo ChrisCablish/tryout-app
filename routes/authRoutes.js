35e9803c811c9b10de3f19be863bc77c1f379113
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2024-06-01 08:32:29 -0500

    authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -57,15 +57,24 @@ router.post("/login", async (req, res) => {
 
     // Assuming session middleware is set up
     req.session.userId = user.id; // Save user id in the session
-    res
-      .status(200)
-      .json({
-        message: "Logged in successfully",
-        user: { id: user.id, email: user.email },
-      });
+    console.log("this is a test"); // Add this line to check session setting
+    res.status(200).json({
+      message: "Logged in successfully",
+      user: { id: user.id, email: user.email },
+    });
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).send({ message: "Failed to log in", error: error.message });
+  }
+});
+
+// GET /check if user is logged in already
+router.get("/check-auth", (req, res) => {
+  if (req.session.userId) {
+    res.status(200).json({ isAuthenticated: true });
+  } else {
+    res.status(200).json({ isAuthenticated: false });
+    console.log("testing");
   }
 });
 
